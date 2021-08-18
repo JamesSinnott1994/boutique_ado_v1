0f7c8e43441eabd211ca8e7a0f9d9480c33e612e
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2021-08-18 12:51:04 +0000

    055 - Deployment part 1

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,6 +125,10 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.parse('postgres://lcwhjohtpfwbsm:00642acf78e7c2a9ca029abf06bc396c0bbe5983d90ef06d566ec9fe4681831f@ec2-54-170-163-224.eu-west-1.compute.amazonaws.com:5432/d351ig0hso3032')
+# }
 
 
 # Password validation
